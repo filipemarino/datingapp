48bdd2b287721ff9fefb4136ef4706536ee7c83a
Filipe Marino <marino.filipe2@gmail.com>
2020-03-25 17:13:11 -0300

    End of section 6

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -1,41 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpInterceptor, HttpRequest, HttpHandler,
+  HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  intercept(
-    req: import('@angular/common/http').HttpRequest<any>,
-    next: import('@angular/common/http').HttpHandler
-  ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
-    return next.handle(req).pipe(
-        catchError(error => {
-            if (error.status === 401) {
-                return throwError(error.statusText);
-            }
-            if (error instanceof HttpErrorResponse) {
-                const applicationError = error.headers.get('Application-Error');
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError(error => {
+                if (error instanceof HttpErrorResponse) {
+                    if (error.status === 401) {
+                        return throwError(error.statusText);
+                    }
+                    const applicationError = error.headers.get('Application-Error');
+                    if (applicationError) {
+                        console.error(applicationError);
+                        return throwError(applicationError);
+                    }
+                    let serverError = error.error;
 
-                if (applicationError) {
-                    return throwError(applicationError);
-                }
+                    if (error.error.errors !== undefined) {
+                      serverError = error.error.errors;
+                    }
 
-                const serverError = error.error;
-                let modalStateErrors = '';
-                if (serverError.errors && typeof serverError.errors === 'object') {
-                    for (const key in serverError.errors) {
-                        if (serverError.errors[key]) {
-                            modalStateErrors += serverError.errors[key] + '\n';
+                    let modalStateErrors = '';
+                    if (serverError && typeof serverError === 'object') {
+                        for (const key in serverError) {
+                            if (serverError[key]) {
+                                modalStateErrors += serverError[key] + '\n';
+                            }
                         }
                     }
+                    return throwError(modalStateErrors || serverError || 'Server Error');
                 }
-
-                return throwError(modalStateErrors || serverError || 'Server Error');
-            }
-        })
-    );
-  }
+            })
+        );
+    }
 }
 
 export const ErrorInterceptorProvider = {
